ef8fed847a52c9acafe9ca4f4e6ecc80f5cd3689
chyngyz <camphosis@gmail.com>
2016-09-09 19:37:22 +0600

    Almost done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+    var $juryItems = $(".hr__jury__desc");
+    $juryItems.each(function(ind, el) {
+        $(el).dotdotdot({
+            height: 50
+        });
+
+        $(el).parent().find('.hr__jury__readmore').click(function(e) {
+            e.preventDefault();
+            $(el).parent().parent().addClass('hr__jury__item--open');
+            $(el).trigger("destroy");
+        });
+
+        $(el).parent().find('.hr__jury__close').click(function(e) {
+            e.preventDefault();
+            $(el).parent().parent().removeClass('hr__jury__item--open');
+            $(el).dotdotdot({
+                height: 50
+            });
+        });
+    })
+    
+});
