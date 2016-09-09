212dddd2bcc5e2c6e088258913d512ffb4a10bd4
Chyngyz Arystan uulu <camphosis@gmail.com>
2016-09-10 00:03:12 +0600

    Build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var $juryItems = $(".hr__jury__desc");
+    var $juryItems = $('.hr__jury__desc');
     $juryItems.each(function(ind, el) {
         $(el).dotdotdot({
             height: 50
@@ -8,7 +8,7 @@ $(document).ready(function(){
         $(el).parent().find('.hr__jury__readmore').click(function(e) {
             e.preventDefault();
             $(el).parent().parent().addClass('hr__jury__item--open');
-            $(el).trigger("destroy");
+            $(el).trigger('destroy');
         });
 
         $(el).parent().find('.hr__jury__close').click(function(e) {
